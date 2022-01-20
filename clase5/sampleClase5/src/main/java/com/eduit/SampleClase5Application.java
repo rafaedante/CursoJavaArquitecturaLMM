20c37ce736d0596694f57e84c06ba0b0b173241f
Rafael Ibarra <rafael.dante@gmail.com>
2022-01-19 22:07:15 -0300

    clase 5

---
+++ clase5/sampleClase5/src/main/java/com/eduit/SampleClase5Application.java
@@ -0,0 +1,13 @@
+package com.eduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleClase5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleClase5Application.class, args);
+	}
+
+}
